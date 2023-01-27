d0f68e8924412cd5a6bced1fd99cdbbba7997d07
aeae <80916217+ae12321@users.noreply.github.com>
2023-01-27 20:07:45 +0900

    add drop sql for SQLTools

---
+++ jhipster.session.sql
@@ -0,0 +1,12 @@
+drop table if exists databasechangelog cascade;
+drop table if exists databasechangeloglock cascade;
+drop table if exists jhi_authority cascade;
+drop table if exists jhi_user cascade;
+drop table if exists jhi_user_authority cascade;
+drop table if exists representative cascade;
+drop table if exists seat cascade;
+drop table if exists reserved_seat cascade;
+drop table if exists seat_group cascade;
+drop table if exists seat_control cascade;
+
+
